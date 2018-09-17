b1eccd9a05f8de209945b0549f3ac040ae22dcaf
Gansca Paul Mihai <gansca.paul@gmail.com>
2018-09-17 17:17:25 +0300

    Complete layout, perfect clone with improved nav

--- js/script.js
+++
@@ -1,9 +0,0 @@
-let hamburger = document.querySelector(".nav-toggle-label");
-
-console.log(hamburger)
-
-function handleNav() {
-    hamburger.classList.toggle('close');
-}
-
-hamburger.addEventListener('click', handleNav);
